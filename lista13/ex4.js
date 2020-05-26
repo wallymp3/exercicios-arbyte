6d82dca9986350d820dd8f7db9382b89bc8eac95
Wallace Pinto <wall_h@hotmail.com>
2020-05-26 11:19:12 -0300

    Lista 13 resolvida

---
+++ lista13/ex4.js
@@ -0,0 +1,21 @@
+/*Faça um algoritmo que calcule a média de potência dos carros da Europe (Europa)
+Similar a atividade 2 porém usaremos o campo Origin para selecionar os carros da
+europa e depois disso aplicar a média em cima do campo Horsepower.*/
+
+var fs = require('fs');
+var json = fs.readFileSync('pack-json/cars.json');
+var cars = JSON.parse(json);
+
+var carrosEuropeus = cars.filter((cars) => cars.Origin == "Europe")
+
+
+
+var media = carrosEuropeus.reduce(function(total, item, indice, array) {
+    total += item.Horsepower
+    if (indice == array.length - 1) {
+        return total / array.length;
+    }
+    return total;
+}, 0)
+
+console.log(`A media de potência é ${media.toFixed(2)}`);
